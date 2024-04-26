a37b1af7214adf63fc656ec2ad7fd545ff2b636c
PerrkMP <perrk.mp@gmail.com>
2024-04-26 15:28:37 +0300

    init

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import QRScanner from "./pages/Scanner";
-import Stream from "./pages/Stream";
+
 
 import "./App.css";
 const App = () => {
@@ -14,8 +13,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/scanner" element={<QRScanner />}></Route>
-        <Route path="/stream" element={<Stream />}></Route>
       </Routes>
     </BrowserRouter>
   );
